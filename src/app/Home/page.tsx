e32cd92446700b8e717969116ff615e0c3d374c8
isaque-cwb <isaque.lourenco2@gmail.com>
2023-12-07 17:23:24 -0300

    chore: refactor header added logout in him

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -18,12 +18,7 @@ export default function Home() {
   const { userAuth, handleStateUserChange, handleClearChange } = useAuthContext()
 
 
-  async function handleLogout() {
-    handleClearChange()
-    window.sessionStorage.removeItem('auth_token')
-    Cookie.remove('auth_token')
-    router.push('/')
-  }
+
 
   useEffect(() => {
     if (!userAuth) {
@@ -43,29 +38,7 @@ export default function Home() {
       <Header />
 
       <div className="flex flex-col w-full h-full place-content-center bg-slate-100  gap-2">
-        <p className="border-2 rounded-md p-1 bg-gray-100">{userAuth?.user.email}</p>
-        <div className="border-2 rounded-md p-1 bg-gray-100">
-          <Link href={"/"}> voltar para Login</Link>
 
-        </div>
-        <div >
-          <button type="button" onClick={handleLogout} className=" w-40 font-bold text-lg pt-2 border-teal-600 border-2 rounded-lg h-12 bg-teal-100 hover:bg-teal-200 transition-all duration-500 flex flex-col items-center shadow-[0_4px_9px_-4px_#0D7E94]" >
-            {isLoading
-              ? <PulseLoader
-                className="mt-2  w-full  flex justify-center"
-                color="#0D7E94"
-                size={10}
-                loading={isLoading}
-
-              />
-              :
-              <span className=" transition duration-300 hover:scale-110  w-full">
-                Sair
-              </span>
-            }
-          </button>
-
-        </div>
       </div>
 
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,24 @@
 import { Box, Image, Wrap, WrapItem } from '@chakra-ui/react'
 import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import PulseLoader from 'react-spinners/PulseLoader'
+import Cookie from 'js-cookie'
+import { useAuthContext } from '@/context/authContext'
 
 export default function Header() {
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+  const { userAuth, handleStateUserChange, handleClearChange } = useAuthContext()
+
+
+  async function handleLogout() {
+    handleClearChange()
+    window.sessionStorage.removeItem('auth_token')
+    Cookie.remove('auth_token')
+    router.push('/')
+  }
+
   return (
     <div className="border-2 flex relative h-auto w-full p-2 justify-between items-center">
       <Box boxSize='fit-conten' >
@@ -15,7 +32,24 @@ export default function Header() {
       </div>
       <div className='flex flex-col items-center border-2 rounded-xl p-2'>
         <Avatar name='Dan Abrahmov' src='avatar.png' />
-        <h1>Olá, <strong>Nome...</strong></h1>
+        <h1 className='text-sm'>Olá, <strong>Nome...</strong></h1>
+        <div >
+          <button type="button" onClick={handleLogout} className="relative w-24 font-bold text-lg mt-1 border-teal-600 border-2 rounded-lg h-6 bg-teal-100 hover:bg-teal-200 transition-all duration-500 flex flex-col items-center shadow-[0_4px_9px_-4px_#0D7E94]" >
+            {isLoading
+              ? <PulseLoader
+                className=" w-full  flex justify-center items-center"
+                color="#0D7E94"
+                size={10}
+                loading={isLoading}
+
+              />
+              :
+              <span className="text-sm transition duration-300 hover:scale-110 ">
+                Sair
+              </span>
+            }
+          </button>
+        </div>
       </div>
 
     </div>
